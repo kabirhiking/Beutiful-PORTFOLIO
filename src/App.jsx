2625cc24474e0a30a8291aa8d6896a4fc4a0feb6
kabir <kabirraihan249@gmail.com>
2025-08-02 15:26:47 +0600

    creating animated loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useState, useTransition } from "react";
 import"./App.css";
 import { LoadingScreen  } from "./components/LoadingScreen";
+import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <>
-      <LoadingScreen />
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} {" "}
+        <div 
+          className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        </div>
     </>
   );
 }
