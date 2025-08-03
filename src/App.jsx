14018145300b4ff5fa5e1a3c9e9ae2694c272401
kabir <kabirraihan249@gmail.com>
2025-08-03 12:59:52 +0600

    building responsive navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState, useTransition } from "react";
 import"./App.css";
 import { LoadingScreen  } from "./components/LoadingScreen";
 import "./index.css";
-
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
+ 
 
   return (
     <>
@@ -12,8 +17,10 @@ function App() {
         <div 
           className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
-        } bg-black text-gray-100`}
-      >
+          } bg-black text-gray-100`}
+        >
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         </div>
     </>
   );
